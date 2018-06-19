f75adadb00b669ca62c40a3be0ff77dac15c889c
JerkaylaN <jerkayla2001@gmail.com>
2018-06-19 19:18:10 +0000

    Done.

---
+++ while_until_loops.rb
@@ -0,0 +1,23 @@
+def wingdarium_leviosa
+  counter = 6
+  
+  while counter < 10 
+  puts "Wingdarium Leviosa"
+  
+  counter = counter + 1
+  end 
+
+end 
+
+wingdarium_leviosa
+
+def wingdarium_leviosa
+  counter = 6
+  
+  until counter == 10 do
+    puts "Wingdarium Leviosa"
+  
+  counter = counter + 1
+  end
+  
+end
